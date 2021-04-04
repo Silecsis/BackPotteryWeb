eb2677821d94bd1e7f5fd3126174687b4c9b7a29
unknown <silecsis@gmail.com>
2021-04-05 00:31:43 +0200

    Guardado 7. CRUD materials sin 1/2 paginacion ni filtro

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -3,110 +3,147 @@
 namespace App\Http\Controllers;
 
 use App\Models\Material;
-use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 /**
- * Controlador del modelo Material.
- * Contiene el CRUD de Material.
- * Socios solo pueden ver.
+ * CRUD de materiales.
+ * Los administradores pueden hacer el CRUD entero.
+ * Los usuarios solo podrán listar los materiales.
+ * Modificación de perfil de cada usuario.
  */
-class MaterialController extends Controller
+class UserController extends Controller
 {
-     /**
-     * Lista todos los usuarios
+    /**
+     * Lista todos los materiales.
+     * Puede acceder los usuarios logados.
      *
-     * @return void
      */
     public function all()
     {
         $materials=Material::all();
-
         return response()->json($materials);
     }
 
     /**
-     * Devuelve un usuario localizado por el id.
+     * Devuelve un material localizado por el id desde la lista de materiales.
+     * Solo puede acceder el tipo administrador.
      *
      */
     public function show($id)
     {
-        $materials = Material::find($id);
+        if(Auth::user()->type=='admin'){
+            $material = Material::find($id);
  
-        if (!$materials) {
+            if (!$material) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Material no encontrado '
+                ], 400);
+            }
+     
             return response()->json([
-                'success' => false,
-                'message' => 'Material no encontrado '
-            ], 400);
+                'success' => true,
+                'data' => $material->toArray()
+            ], 200);
+
+        }else{
+            return response()->json(['error' => 'Unauthorised'], 401);
         }
- 
-        return response()->json([
-            'success' => true,
-            'data' => $materials->toArray()
-        ], 200);
+        
     }
 
+
     /**
-     * Edita el usuario
+     * Edita el material desde la lista de materiales.
+     * Solo puede acceder el tipo administrador.
      *
      */
     public function update(Request $request, $id)
     {
-        $material = Material::find($id);
+        if(Auth::user()->type=='admin'){
+            $material = Material::find($id);
  
-        if (!$material) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Material no encontrado'
-            ], 400);
+            if (!$material) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Material no encontrado'
+                ], 400);
+            }
+
+            //Si existe, primero validamos.   
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'type_material' => 'required|string|max:255',
+                'temperature' => 'required|int',
+                'toxic' => 'required|boolean'
+             ]); 
+        
+            $updated = $material->fill($request->all())->save();
+        
+            if ($updated)
+                return response()->json([
+                    'success' => true
+                ]);
+            else
+                return response()->json([
+                    'success' => false,
+                    'message' => 'El material no puede ser actualizado'
+                ], 500);
+
+        }else{
+            return response()->json(['error' => 'Unauthorised'], 401);
         }
- 
-        $updated = $material->fill($request->all())->save();
- 
-        if ($updated)
-            return response()->json([
-                'success' => true
-            ]);
-        else
-            return response()->json([
-                'success' => false,
-                'message' => 'El material no puede ser actualizado'
-            ], 500);
+        
     }
 
+
+    /**
+     * Crea un nuevo material. 
+     * Opción desde la vista de listar materiales.
+     * Solo puede acceder el tipo administrador.
+     *
+     */
     public function create(Request $request)
     {
-        $request->validate([
-            'name' => 'required',
-            'type_material' => 'required',
-            'temperature' => 'required',
-            'toxic' => 'required',
-         ]);
- 
-        $material = new Material();
-        $material->name = $request->name;
-        $material->type_material = $request->type_material;
-        $material->temperature = $request->temperature;
-        $material->toxic = $request->toxic;
-        $material->created_at = Carbon::now()->format('Y-m-d H:i:s');
-        $material->updated_at = Carbon::now()->format('Y-m-d H:i:s');
- 
-        if ($material->save())
-            return response()->json([
-                'success' => true,
-                'data' => $material->toArray()
-            ]);
-        else
-            return response()->json([
-                'success' => false,
-                'message' => 'Se ha producido un error a la hora de crear el material'
-            ], 500);
+        if(Auth::user()->type=='admin'){
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'type_material' => 'required|string|max:255',
+                'temperature' => 'required|int',
+                'toxic' => 'required|boolean'
+             ]); 
+
+            
+            $material = new Material();
+            $material->name = $request->name;
+            $material->type_material = $request->type_material;
+            $material->temperature = $request->temperature;
+            $material->toxic = $request->toxic;
+            $material->created_at = Carbon::now()->format('Y-m-d H:i:s');
+            $material->updated_at = Carbon::now()->format('Y-m-d H:i:s');
+            
+            if ($material->save())
+                return response()->json([
+                    'success' => true,
+                    'data' => $material->toArray()
+                ]);
+            else
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Se ha producido un error a la hora de crear el material'
+                ], 500);
+        }else{
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
     }
 
     /**
-     * Elimina a un usuario de la bbdd.
+     * Elimina a un material desde la lista de materiales.
+     * Solo puede acceder el tipo administrador.
      */
     public function destroy($id)
     {
